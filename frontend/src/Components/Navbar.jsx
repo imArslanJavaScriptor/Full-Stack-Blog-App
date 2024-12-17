6aa2bbefcba4d3e9de7a32f38bae772a9ac59b17
Muhammad Arslan <imarslan7061@gmail.com>
2024-12-17 23:05:24 +0500

    Preparing

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div className='w-full bg-blue-500 p-5'>Navbar</div>
+    <div className='w-full bg-[#0c0c0c] p-5 flex items-center justify-between'>
+      <div className="logo text-4xl font-bold text-white">Arslan Blogs</div>
+      <div className='flex items-center gap-4 text-lg font-medium text-white hover:text-white/80'>
+      <NavLink className={``}>Home</NavLink>
+      <NavLink>About</NavLink>
+      <NavLink>Blogs</NavLink>
+      <NavLink>Services</NavLink>
+      <NavLink>Contact</NavLink>
+      </div>
+    </div>
   )
 }
 
